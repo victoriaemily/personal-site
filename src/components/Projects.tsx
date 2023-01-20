dc5c6252fa8ef8558e6e0f2a162b018b0517f7f9
vakiko <victoria.c6140@gmail.com>
2023-01-20 16:45:00 -0600

    changes for vercel deployment

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -9,7 +9,7 @@ const Projects = () => {
             <figure><img src="../../tests.jpg" alt="Shoes" /></figure>
             <div className="card-body bg-white rounded-lg">
                 <h2 className="card-title">to-do list</h2>
-                <p>this to-do list uses a mongodb backend, ran on the cloud on mongo atlas, with a react & html/css frontend</p>
+                <p>{"this to-do list uses a mongodb backend, ran on the cloud on mongo atlas, with a react & html/css frontend"}</p>
                 <div className="card-actions justify-end">
                 <a href="https://github.com/victoriaemily/todoList" className="btn gap-3 bg-gray-600 text-2xl text-white"><AiFillGithub/></a>
                 </div>
@@ -20,7 +20,7 @@ const Projects = () => {
             <figure><img src="../../website2.jpg" alt="Shoes" /></figure>
             <div className="card-body bg-white rounded-lg">
                 <h2 className="card-title">victoria's site</h2>
-                <p>a project using tailwind css, typescript, & react for the frontend and next.js for the backend framework</p>
+                <p>{"a project using tailwind css, typescript, & react for the frontend and next.js for the backend framework"}</p>
                 <div className="card-actions justify-end">
                 <a href="https://github.com/victoriaemily/personal-site" className="btn gap-3 bg-gray-600 text-2xl text-white"><AiFillGithub/></a>
                 </div>
